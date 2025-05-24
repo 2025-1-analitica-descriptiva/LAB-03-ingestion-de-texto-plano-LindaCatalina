630ad2945d09d8824d7b6eaea92048de10de091e
Linda Catalina Correa Lozano <licorrea@unal.edu.co>
2025-05-24 11:06:46 -0500

    Laboratorio 3

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -18,3 +18,45 @@ def pregunta_01():
 
 
     """
+    import pandas as pd
+
+    with open('./files/input/clusters_report.txt', 'r', encoding="utf-8") as f:
+        lines = f.read().replace('%', '').splitlines()
+        
+    cluster_row = []
+    current_entry = []
+
+    for line in lines[4:]:
+        if line.strip():
+            current_entry.append(line.strip())
+        else:
+            if current_entry:
+                cluster_row.append(" ".join(current_entry))
+                current_entry = []
+
+    cluster_data = []
+    for row in cluster_row:
+        columns_data = row.split()
+        col0 = int(columns_data[0])
+        col1 = int(columns_data[1])
+        col2 = float(columns_data[2].replace(',', '.'))
+        col3 = (
+            " ".join(columns_data[3:])
+            .replace(' ,', ',')
+            .replace(', ', ', ')
+            # .strip('%')
+            .rstrip('.')
+            .strip())
+        cluster_data.append({
+                    "cluster": col0,
+                    "cantidad_de_palabras_clave": col1,
+                    "porcentaje_de_palabras_clave": col2,
+                    "principales_palabras_clave": col3,
+            })
+                                                    
+    result = pd.DataFrame(cluster_data)
+
+    return(result)
+
+if __name__ == '__main__':
+    print(pregunta_01())
